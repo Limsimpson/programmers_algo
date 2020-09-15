2a88a3a95efe2f3ff5721e14db3c6ea8f36b8172
simpson <hisimpson90@gmail.com>
2020-09-15 14:36:33 +0900

    2016년 업로드

--- level1/2016년.js
+++ level1/2016년.js
@@ -2,13 +2,10 @@ const solution = (a, b) => {
   let daysOfWeek = ["FRI", "SAT", "SUN", "MON", "TUE", "WED", "THU"];
   let numOfDays = [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
 
-  let sumDays = numOfDays
-    .map((days, idx) => {
-      return idx + 1 < a && days;
-    })
-    .reduce((c, d) => {
-      return c + d;
-    });
+  let sumDays = numOfDays.reduce((acc, curr, idx) => {
+    if (a === 1) return 0;
+    return idx + 1 < a ? acc + curr : acc + 0;
+  });
 
   return daysOfWeek[(sumDays + b - 1) % 7];
 };
