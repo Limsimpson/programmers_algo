50866bc06c6dd2f215cce95fcc2598536f7f9dbc
simpson <hisimpson90@gmail.com>
2020-09-16 18:10:18 +0900

    주석 처리

--- level1/문자열을정수로바꾸기.js
+++ level1/문자열을정수로바꾸기.js
@@ -3,6 +3,6 @@ const solution = (s) => {
 };
 
 // 동적 언어인 Javascript가 문자와 숫자를 사칙연산 할 경우 숫자를 반환하는 성격을 이용함.
-return s / 1;
-return +s;
-return Number(s);
+// return s / 1;
+// return +s;
+// return Number(s);
