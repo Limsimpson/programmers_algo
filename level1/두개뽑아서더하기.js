6fa3624da41d610f16ed044636653c32b457c861
simpson <hisimpson90@gmail.com>
2020-09-16 17:58:13 +0900

    코드 정리

--- level1/두개뽑아서더하기.js
+++ level1/두개뽑아서더하기.js
@@ -8,7 +8,20 @@ const solution = (numbers) => {
     });
   }
 
-  return answer.sort((a, b) => {
-    return a - b;
-  });
+  return answer.sort((a, b) => a - b);
 };
+
+// for문 사용
+// function solution(numbers) {
+//   var answer = [];
+
+//   for (let i = 0; i < numbers.length - 1; i++) {
+//     for (let j = i + 1; j < numbers.length; j++) {
+//       if (answer.indexOf(numbers[i] + numbers[j]) === -1) {
+//         answer.push(numbers[i] + numbers[j]);
+//       }
+//     }
+//   }
+
+//   return answer.sort((a, b) => a - b);
+// }
