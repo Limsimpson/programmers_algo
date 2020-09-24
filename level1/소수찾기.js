5f253d879eecc58bfd5d22220f56453ce0ee87d5
simpson <hisimpson90@gmail.com>
2020-09-25 01:07:56 +0900

    소수 찾기 풀이 업로드

--- level1/소수찾기.js
+++ level1/소수찾기.js
@@ -1,23 +1,58 @@
+// 두 번째 풀이
 const solution = (n) => {
-  function isDecimal(n) {
-    let value = true;
-    for (let i = 2; i <= n; i++) {
-      for (let j = 2; j <= i - 1; j++) {
-        if (i % j === 0) {
-          value = false;
-        }
+  let sieve = [];
+  let primes = [];
+
+  for (let i = 2; i <= n; i++) {
+    if (!sieve[i]) {
+      primes.push(i);
+      for (let j = 2 * i; j <= n; j += i) {
+        sieve[j] = true;
       }
     }
-    return value;
   }
-
-  let answer = 0;
-  for (let k = 2; k <= n; k++) {
-    if (isDecimal(k)) {
-      answer += 1;
-    }
-    console.log(isDecimal(k));
-  }
-
-  return answer;
+  return primes.length;
 };
+
+// 첫 번째 풀이//////////////////////////
+// const solution = (n) => {
+//   let answer = 0;
+//   let nArray = [];
+
+//   for (let i = 2; i <= n; i++) {
+//     nArray[i] = i;
+//   }
+
+//   for (let j = 2; j <= n; j++) {
+//     if (nArray[j] !== 0) {
+//       for (let k = j + j; k <= n; k = k + j) {
+//         nArray[k] = 0;
+//       }
+//     }
+//   }
+
+//   for (let z = 2; z < nArray.length; z++) {
+//     if (nArray[z] !== 0) {
+//       answer += 1;
+//     }
+//   }
+
+//   return answer;
+// };
+
+// 10, 11, 12 시간 초과////////////////////////
+// const solution = (n) => {
+//     let answer = [];
+
+//     for(let i = 1; i <= n; i++) {
+//         let isDecimal = true;
+//         for(let j = 2; j < i; j++) {
+//             if(i % j === 0) {
+//                 isDecimal = false;
+//             }
+//         }
+//         isDecimal && answer.push(i);
+//     }
+
+//     return answer.length - 1;
+// }
