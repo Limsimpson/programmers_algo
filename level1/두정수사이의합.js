f481f2ccf085852f4c3ddbb9398604b5786af3ab
simpson <hisimpson90@gmail.com>
2020-09-30 19:40:17 +0900

    두 정수 사이의 합 업로드

---
+++ level1/두정수사이의합.js
@@ -0,0 +1,10 @@
+const solution = (a, b) => {
+  let answer = 0;
+  for (let i = Math.min(a, b); i <= Math.max(a, b); i++) {
+    answer += i;
+  }
+  return answer;
+};
+
+// 짧은 풀이////////////////
+// return ((a + b) * (Math.abs(b - a) + 1)) / 2;
