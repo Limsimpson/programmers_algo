528b7ff264351f9870c4b81090a16283848e1a4c
simpson <hisimpson90@gmail.com>
2020-10-05 21:35:16 +0900

    풀이 fix

--- level1/이상한문자만들기.js
+++ level1/이상한문자만들기.js
@@ -20,23 +20,23 @@ const solution = (s) => {
 // \w는 word를 표현하며 알파벳 + 숫자 + _중의 한 문자를 의미
 // g는 global을 표현하며 대상 문자열 내에 모든 패턴을 검색하는 것을 의미
 
-// 테스트는 통과 채점은 1, 2, 6번만 통과 왜그런지 같이 고민하려고 가져옴
-const solution = (s) => {
-  let answer = "";
-  let idx = 0;
-  for (let i = 0; i < s.length; i++) {
-    if (s[i] === " ") {
-      answer += " ";
-      idx = 0;
-    } else {
-      if (i % 2 === 0) {
-        answer += s[i].toUpperCase();
-        idx++;
-      } else {
-        answer += s[i].toLowerCase();
-        idx++;
-      }
-    }
-  }
-  return answer;
-};
+// 시간, 용량 가장 적음
+// const solution = (s) => {
+//   let answer = "";
+//   let idx = 0;
+//   for (let i = 0; i < s.length; i++) {
+//     if (s[i] === " ") {
+//       answer += " ";
+//       idx = 0;
+//     } else {
+//       if (idx % 2 === 0) {
+//         answer += s[i].toUpperCase();
+//         idx++;
+//       } else {
+//         answer += s[i].toLowerCase();
+//         idx++;
+//       }
+//     }
+//   }
+//   return answer;
+// };
