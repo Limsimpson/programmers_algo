a50b7861d4694eeabefc8bc1dadd8afe363f78fa
simpson <hisimpson90@gmail.com>
2020-10-07 10:06:24 +0900

    평균구하기 풀이 추가

--- level1/평균구하기.js
+++ level1/평균구하기.js
@@ -1,5 +1,17 @@
-function solution(arr) {
-  const reducer = (accumulator, currentValue) => accumulator + currentValue;
-  const answer = arr.reduce(reducer) / arr.length;
-  return answer;
-}
+// reduce를 이용한 풀이
+const solution = (arr) => {
+  return (
+    arr.reduce((acc, curr) => {
+      return acc + curr;
+    }) / arr.length
+  );
+};
+
+// for문을 이용한 풀이
+const solution = (arr) => {
+  let answer = 0;
+  for (let i = 0; i < arr.length; i++) {
+    answer += arr[i];
+  }
+  return answer / arr.length;
+};
