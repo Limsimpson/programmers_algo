673799bd21e54fc7b9eecebf9154374f50f9a28f
simpson <hisimpson90@gmail.com>
2020-10-13 00:14:27 +0900

    3진법 뒤집기 업로드

---
+++ level1/3진법뒤집기.js
@@ -0,0 +1,13 @@
+const solution = (n) => {
+  return parseInt(n.toString(3).split("").reverse().join(""), 3);
+};
+
+// 다른 풀이 ///////////////////////////////
+// function solution(n) {
+//   const answer = [];
+//   while (n !== 0) {
+//     answer.unshift(n % 3);
+//     n = Math.floor(n / 3);
+//   }
+//   return answer.reduce((acc, v, i) => acc + v * Math.pow(3, i), 0);
+// }
