b74e8ee4a456417ed472eead29a6c95521532429
simpson <hisimpson90@gmail.com>
2020-10-21 15:39:38 +0900

    최대공약수와 최소공배수 풀이 추가

--- level1/최대공약수와최소공배수.js
+++ level1/최대공약수와최소공배수.js
@@ -25,3 +25,47 @@ const solution = (n, m) => {
 
   return answer;
 };
+
+// 효율적이고 짧은 코드 ////////////////////
+const solution = (n, m) => {
+  function greatestCommonDivisor(n, m) {
+    return m ? greatestCommonDivisor(m, n % m) : Math.abs(n);
+  }
+
+  function leastCommonMultipleOfTwo(n, m) {
+    return (n * m) / greatestCommonDivisor(n, m);
+  }
+
+  function gcdlcm(n, m) {
+    return [greatestCommonDivisor(n, m), leastCommonMultipleOfTwo(n, m)];
+  }
+
+  return gcdlcm(n, m);
+};
+
+// 더 짧은 코드 //////////////////////////
+const solution = (n, m) => {
+  function gcdlcm(n, m) {
+    var r;
+    // 뭐라는...건지...
+    for (var nm = n * m; (r = n % m); n = m, m = r) {}
+    return [m, nm / m];
+  }
+  return gcdlcm(n, m);
+};
+
+// 또 다른 풀이 ////////////////////////////
+const solution = (n, m) => {
+  function calc_gcd(n, m) {
+    if (m == 0) return n;
+    return n > m ? calc_gcd(m, n % m) : calc_gcd(n, m % n);
+  }
+
+  function gcdlcm(n, m) {
+    let gcd = calc_gcd(n, m);
+    let lcm = (n * m) / gcd;
+    return [gcd, lcm];
+  }
+
+  return gcdlcm(n, m);
+};
