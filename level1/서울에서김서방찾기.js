915f3028af6e57027d54ef6209358241a257b26f
simpson <hisimpson90@gmail.com>
2020-11-11 19:35:20 +0900

    서울에서 김서방 찾기 재 업로드

--- level1/서울에서김서방찾기.js
+++ level1/서울에서김서방찾기.js
@@ -1,9 +1,16 @@
+// for문을 이용한 풀이
+const solution = (seoul) => {
+  let answer;
+  for (let i = 0; i < seoul.length; i++) {
+    if (seoul[i] === "Kim") {
+      answer = i;
+      break;
+    }
+  }
+  return `김서방은 ${answer}에 있다`;
+};
+
+// indexOf를 이용한 풀이
 const solution = (seoul) => {
   return `김서방은 ${seoul.indexOf("Kim")}에 있다`;
 };
-
-// 처음 풀이 /////////////////////
-function solution(seoul) {
-  var answer = seoul.indexOf("Kim");
-  return "김서방은 " + answer + "에 있다";
-}
