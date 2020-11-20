afe193fe26a460675d3ad5238ec0750b3a30e10b
simpson <hisimpson90@gmail.com>
2020-11-20 17:03:17 +0900

    타겟넘버 이름 변경, 변수명 변경

--- level2/타겟넘버.js
+++ level2/타겟넘버.js
@@ -16,14 +16,14 @@ const solution = (numbers, target) => {
 // 중간
 const solution = (numbers, target) => {
   return getAnswer(numbers, 0, target);
-  function getAnswer(num, r, tgt) {
+  function getAnswer(num, val, tgt) {
     if (num.length > 0) {
-      let tmp = num.pop() + r;
+      let tmp = num.pop() + val;
       return (
         getAnswer(num.slice(), tmp, tgt) + getAnswer(num.slice(), -tmp, tgt)
       );
     } else {
-      if (r === tgt) return 1;
+      if (val === tgt) return 1;
       else return 0;
     }
   }
@@ -31,30 +31,30 @@ const solution = (numbers, target) => {
 
 // 가장 느림
 const solution = (numbers, target) => {
-  let count = 0;
+  let answer = 0;
   let arr = [];
-  let arrLength = Math.pow(2, numbers.length + 1);
+  let numLen = Math.pow(2, numbers.length + 1);
   let startPoint = Math.pow(2, numbers.length);
-  let j = 0;
+  let idx = 0;
   let buff = 3;
 
-  for (let i = 0; i < arrLength; i++) {
+  for (let i = 0; i < numLen; i++) {
     if (i === 0) {
       arr.push(0);
     } else {
-      if (i % 2 == 0) {
-        arr[i - 1] = arr[parseInt((i - 1) / 2)] - numbers[j];
-        arr[i] = arr[parseInt((i - 1) / 2)] + numbers[j];
+      if (i % 2 === 0) {
+        arr[i - 1] = arr[parseInt((i - 1) / 2)] - numbers[idx];
+        arr[i] = arr[parseInt((i - 1) / 2)] + numbers[idx];
       }
-      if (i == buff - 1) {
-        buff = Math.pow(2, ++j + 2) - 1;
+      if (i === buff - 1) {
+        buff = Math.pow(2, ++idx + 2) - 1;
       }
     }
   }
 
-  for (let i = startPoint; i < arrLength; i++) {
-    if (target === arr[i]) count++;
+  for (let i = startPoint; i < numLen; i++) {
+    if (target === arr[i]) answer++;
   }
 
-  return count;
+  return answer;
 };
