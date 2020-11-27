0eef5f2f648b2cd25ade3e8e98f87dc15b8d0f99
simpson <hisimpson90@gmail.com>
2020-11-27 20:46:53 +0900

    가장 큰 수 재업로드

--- level2/가장 큰 수.js
+++ level2/가장 큰 수.js
@@ -1,6 +1,20 @@
+// 두 번째 풀이
+const solution = (numbers) => {
+  let answer = "";
+  numbers = numbers.sort((a, b) => {
+    let strA = a.toString();
+    let strB = b.toString();
+    return strB + strA - (strA + strB);
+  });
+  answer = numbers[0] === 0 ? "0" : numbers.join("");
+  return answer;
+};
+
+// 첫 번째 풀이
+// 이게 약간 더 빠름
 const solution = (numbers) => {
   let combinedNum = numbers
     .sort((x, y) => +("" + y + x) - +("" + x + y))
     .join("");
-  return combinedNum <= 0 ? "0" : combinedNum.toString();
+  return combinedNum <= 0 ? "0" : combinedNum;
 };
