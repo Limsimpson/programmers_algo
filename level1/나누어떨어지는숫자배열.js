856758df25491b21bc8a038e3dbd943bd7736172
simpson <hisimpson90@gmail.com>
2020-11-30 16:26:27 +0900

    나누어 떨어지는 숫자 배열 재업로드

--- level1/나누어떨어지는숫자배열.js
+++ level1/나누어떨어지는숫자배열.js
@@ -1,6 +1,19 @@
-function solution(arr, divisor) {
+// 두 번째 풀이
+// 시간은 첫 번째 풀이와 비슷함
+const solution = (arr, divisor) => {
+  let answer = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] % divisor === 0) answer.push(arr[i]);
+  }
+  if (answer.length <= 0) answer.push(-1);
+  else answer.sort((a, b) => a - b);
+  return answer;
+};
+
+// 첫 번째 풀이
+const solution = (arr, divisor) => {
   arr = arr.filter((el) => {
     return el % divisor === 0;
   });
   return arr.length > 0 ? arr.sort((a, b) => a - b) : [-1];
-}
+};
