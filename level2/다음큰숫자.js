222fb9cd4e542f6283086e462c6af21a2d93617b
simpson <hisimpson90@gmail.com>
2020-12-01 15:48:03 +0900

    다음 큰 숫자 풀이 추가

--- level2/다음큰숫자.js
+++ level2/다음큰숫자.js
@@ -17,7 +17,31 @@ const solution = (n) => {
   }
 };
 
-//match와 while을 사용한 풀이 (가장 빠름)
+// 시프트 연산자 풀이
+// 알고리즘 시간 복잡도 O(log n) 이지만 사실상 O(a)에 가까움.
+// 655395 입력후 1억번 실행에 소요 시간450ms 미만
+const solution = (n) => {
+  let i, j;
+  // 1을 찾을때까지 우로 쉬프트, 쉬프트 횟수 = i
+  for (i = 0; !(n & 1); i++) {
+    n = n >> 1;
+  }
+  // 0을 찾을때까지 우로 쉬프트, 1의 갯수 = j
+  for (j = 0; n & 1; i++, j++) {
+    n = n >> 1;
+  }
+  // 0자리에 1대입, 1의 갯수 -1, i === j 가 될때까지 죄로 쉬프트하면서 쉬프트 횟수 -1
+  for (j--, n++; i !== j; i--) {
+    n = n << 1;
+  }
+  // i === 0 될때까지 좌로 쉬프트 하면서 쉬프트 횟수 -1, 0자리에 1대입
+  for (i; i; i--, n++) {
+    n = n << 1;
+  }
+  return n;
+};
+
+// match와 while을 사용한 풀이 재귀보다 빠름
 const solution = (n) => {
   let giveValLen = n.toString(2).match(/1/g).length;
 
@@ -26,7 +50,7 @@ const solution = (n) => {
   }
 };
 
-// match와 재귀를 활용한 풀이 (빠름)
+// match와 재귀를 활용한 풀이
 const solution = (n, comparisonVal = n + 1) => {
   let giveValLen = n.toString(2).match(/1/g).length;
   let comparisonValLen = comparisonVal.toString(2).match(/1/g).length;
