a54c001e84803174b8cbdacba8d7e417fe2d05e1
simpson <hisimpson90@gmail.com>
2020-12-01 15:43:01 +0900

    가장 큰 수 이름 변경, 다음 큰 숫자 풀이 추가, 변수명 변경

--- level2/다음큰숫자.js
+++ level2/다음큰숫자.js
@@ -6,13 +6,31 @@ const solution = (n) => {
       .filter((el) => el === "1").length;
   }
 
-  let giveVal = NumOfOne(n);
+  let giveValLen = NumOfOne(n);
 
   for (let i = 1; true; i++) {
-    let comparisonVal = NumOfOne(n + i);
+    let comparisonValLen = NumOfOne(n + i);
 
-    if (giveVal === comparisonVal) {
+    if (giveValLen === comparisonValLen) {
       return n + i;
     }
   }
+};
+
+//match와 while을 사용한 풀이 (가장 빠름)
+const solution = (n) => {
+  let giveValLen = n.toString(2).match(/1/g).length;
+
+  while (n++) {
+    if (giveValLen === n.toString(2).match(/1/g).length) return n;
+  }
+};
+
+// match와 재귀를 활용한 풀이 (빠름)
+const solution = (n, comparisonVal = n + 1) => {
+  let giveValLen = n.toString(2).match(/1/g).length;
+  let comparisonValLen = comparisonVal.toString(2).match(/1/g).length;
+
+  if (giveValLen === comparisonValLen) return comparisonVal;
+  else return solution(n, comparisonVal + 1);
 };
