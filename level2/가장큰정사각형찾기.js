3743f41fe3aa2ea89ef7d726d2249ebae1e27470
simpson <hisimpson90@gmail.com>
2020-12-18 22:36:08 +0900

    가장 큰 정사각형 찾기 변수 변경

--- level2/가장큰정사각형찾기.js
+++ level2/가장큰정사각형찾기.js
@@ -1,16 +1,19 @@
 const solution = (board) => {
   let edge = 0;
-  let row = board.length;
-  let col = board[0].length;
+  const row = board.length;
+  const col = board[0].length;
 
   if (row === 1 && col === 1) edge = 1;
 
-  for (let i = 1; i < row; i++) {
-    for (let j = 1; j < col; j++) {
-      if (board[i][j] !== 0) {
-        let min = Math.min(board[i - 1][j - 1], board[i - 1][j]);
-        board[i][j] = Math.min(min, board[i][j - 1]) + 1;
-        edge = Math.max(edge, board[i][j]);
+  for (let row_i = 1; row_i < row; row_i++) {
+    for (let col_i = 1; col_i < col; col_i++) {
+      if (board[row_i][col_i] !== 0) {
+        let min = Math.min(
+          board[row_i - 1][col_i - 1],
+          board[row_i - 1][col_i]
+        );
+        board[row_i][col_i] = Math.min(min, board[row_i][col_i - 1]) + 1;
+        edge = Math.max(edge, board[row_i][col_i]);
       }
     }
   }
