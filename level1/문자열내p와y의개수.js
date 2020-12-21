aec0978b83c1180be9fd06e91867c4159d63ed9d
simpson <hisimpson90@gmail.com>
2020-12-21 15:13:42 +0900

    문자열 내 P와 Y의 개수 재 업로드 / .gitignore 추가

--- level1/문자열내p와y의개수.js
+++ level1/문자열내p와y의개수.js
@@ -1,7 +1,19 @@
+// 세 번째 풀이
+const solution = (s) => {
+  let num = 0;
+  s = s.toUpperCase();
+  for (let i = 0; i < s.length; i++) {
+    if (s[i] === 'P') num++;
+    else if (s[i] === 'Y') num--;
+  }
+  return num === 0 ? true : false;
+};
+
+// 두 번째 풀이
 const solution = (s) => {
   let upperS = s.toUpperCase();
-  let numP = upperS.split("P").length;
-  let numY = upperS.split("Y").length;
+  let numP = upperS.split('P').length;
+  let numY = upperS.split('Y').length;
 
   if (numP === 0 && numY === 0) {
     return true;
@@ -10,27 +22,16 @@ const solution = (s) => {
   }
 };
 
-// 정규식 풀이
-const solution = (s) =>
-  (s.match(/p/gi) || []).length == (s.match(/y/gi) || []).length;
-
-// p는 ++ y는 -- 로 더하고 빼는 법
-var num = 0;
-for (var i = 0; i < s.length; i++) {
-  if (s[i] === "P") num++;
-  if (s[i] === "Y") num--;
-}
-
-// 나의 첫 풀이
+// 첫 번째 풀이
 const solution = (s) => {
   let numP = 0;
   let numY = 0;
   let answer = true;
 
   for (let i = 0; i < s.length; i++) {
-    if (s[i] === "p" || s[i] === "P") {
+    if (s[i] === 'p' || s[i] === 'P') {
       numP += 1;
-    } else if (s[i] === "y" || s[i] === "Y") {
+    } else if (s[i] === 'y' || s[i] === 'Y') {
       numY += 1;
     }
   }
@@ -41,3 +42,7 @@ const solution = (s) => {
   }
   return answer;
 };
+
+// 정규식 풀이
+const solution = (s) =>
+  (s.match(/p/gi) || []).length == (s.match(/y/gi) || []).length;
