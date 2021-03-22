6286ab907429d739305f684d73e1c85c66483291
Limsimpson <hisimpson90@gmail.com>
2021-03-22 12:24:20 +0900

    폰켓몬 풀이 추가

--- level1/폰켓몬.js
+++ level1/폰켓몬.js
@@ -1,3 +1,4 @@
+// 정렬을 이용한 풀이
 function solution(nums) {
   let picks = [];
   nums = nums.sort((a, b) => a - b);
@@ -10,4 +11,12 @@ function solution(nums) {
       }
   }
   return picks.length;
+}
+
+// 중복 제거를 이용한 풀이
+function solution(nums) {
+  const newNums = new Set(nums);
+  const numLen = nums.length / 2;
+  const newNumsLen = newNums.size;
+  return newNumsLen > numLen ? numLen : newNumsLen;
 }
